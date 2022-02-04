44f98b696452a28e2d304fda2472499725462514
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-03 21:12:01 -0300

    Import simulador

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Simulador } from './Components/Simulador/Simulador';
 
 function App() {
   return (
     <div className="App">
       <h1>Simulador de Investimentos</h1>
+      <Simulador />
     </div>
   );
 }
