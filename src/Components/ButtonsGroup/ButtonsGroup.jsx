dc51c808e48c1c908a351ed7964d1c93be343cbf
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-07 20:52:47 -0300

    Import style, Import img

--- src/Components/ButtonsGroup/ButtonsGroup.jsx
+++ src/Components/ButtonsGroup/ButtonsGroup.jsx
@@ -1,10 +1,11 @@
-import info from '../assets/images/info.png';
+import './style.css';
+import info from '../../assets/images/info.png';
 
 export const ButtonsGroup = ({ label, buttons }) => {
   return (
     <div>
       <label>{label}
-        {/* <img src={info} alt="info" /> */}
+        <img className="info" src={info} alt="info" />
       </label>
       {buttons.map((button, i) => (
         <button name={button.name} id={button.id} onClick={button.onClick} key={i} >
