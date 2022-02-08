97072767c93b4a4dcb2d4882a162632cd70322f7
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-08 16:43:47 -0300

    Pass function as props, Add type button

--- src/Components/ButtonsGroup/ButtonsGroup.jsx
+++ src/Components/ButtonsGroup/ButtonsGroup.jsx
@@ -1,14 +1,14 @@
 import './style.css';
 import info from '../../assets/images/info.png';
 
-export const ButtonsGroup = ({ label, buttons }) => {
+export const ButtonsGroup = ({ label, buttons, handleClick }) => {
   return (
     <div>
       <label className="btn-group-label">{label}
         <img className="info" src={info} alt="info" />
       </label>
       {buttons.map((button, i) => (
-        <button name={button.name} id={button.id} onClick={button.onClick} key={i} >
+        <button type="button" onClick={handleClick} name={button.name} id={button.id} onClick={button.onClick} key={i} >
           {button.children}
         </button>
       ))}
