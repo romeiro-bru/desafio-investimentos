9520bbdc1d4d3d545c90ded54f1abbcb48d13eeb
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-09 19:15:56 -0300

    Add usecallback, Set default btn

--- src/pages/home/simulator.jsx
+++ src/pages/home/simulator.jsx
@@ -1,32 +1,36 @@
 import axios from 'axios';
 import "./style.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { ButtonsGroup } from '../../Components/ButtonsGroup/ButtonsGroup';
 import { Input } from '../../Components/Input/Input';
+import { Results } from './results';
 
 const buttonsRendimento = [
   { id: 0, name: "bruto", children: "Bruto" },
   { id: 1, name: "liquido", children: "Líquido", }
 ]
-
 const buttonsIndex = [
-  { id: 0, name: "pre", onClick: () => console.log("pré"), children: "PRÉ" },
-  { id: 1, name: "pos", onClick: () => console.log("pós"), children: "POS", }
+  { id: 0, name: "pre", children: "PRÉ" },
+  { id: 1, name: "pos", children: "POS", }
 ]
-
 const inputFields = [
   { id: 0, name: "aporte-inicial", children: "Aporte Inicial" },
   { id: 0, name: "aporte-mensal", children: "Aporte Mensal" },
   { id: 0, name: "prazo", children: "Prazo (em meses)" },
   { id: 0, name: "rentabilidade", children: "Rentabilidade" },
 ]
+const defaultBtnRendimento = "bruto"
+const defaultButtons = [{}, {}]
 
 export const Simulator = () => {
   const [indicadores, setIndicadores] = useState([])
   const [inputs, setInputs] = useState({})
+  const [selectedBtn, setSelectedBtn] = useState(defaultBtnRendimento)
+  const [selectedButtons, setSelectedButtons] = useState(defaultButtons)
+  const [simulationResult, setSimulationResult] = useState([])
 
   useEffect(() => {
-    const fetch = async () => {
+    const fetchIndicators = async () => {
       try {
         const response = await axios.get("http://localhost:3000/indicadores")
         setIndicadores(response.data)
@@ -34,40 +38,44 @@ export const Simulator = () => {
         console.log(error)
       }
     }
-    fetch()
+    fetchIndicators()
   }, [])
 
-  //add usecallback
-  const handleInputChange = (e) => {
+  const handleInputChange = useCallback((e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value })
-  }
-
+  }, [inputs]
+  )
   const handleClick = (e) => {
     e.preventDefault()
-    console.log(2, e.target.name)
+    console.log(e.target)
+    // setSelectedBtn(e.target.name)
+    if (selectedBtn !== e.target.name) {
+      setSelectedBtn(e.target.name);
+    }
   }
 
-  //add usecallback
   //add validation, only make call if input fields are not empty
   //set button'bruto' and 'pos' as default
-  const handleSubmit = (e) => {
+  const handleSubmit = useCallback((e) => {
     e.preventDefault()
-    const fetch = async () => {
+    const fetchSimulation = async () => {
       try {
         const response = await axios.get("http://localhost:3000/simulacoes")
-        console.log(response.data.filter(x => x.tipoIndexacao === "pre"))
+        setSimulationResult(response.data)
+        // console.log(3, simulations.filter(x => x.tipoIndexacao === selectedBtn))
       } catch (error) {
         console.log(error)
       }
     }
-    fetch()
-  }
+    fetchSimulation()
+  }, [simulationResult])
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Simulador</h2>
       <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-        <ButtonsGroup handleClick={handleClick} buttons={buttonsRendimento} label="Rendimento" />
-        <ButtonsGroup handleClick={handleClick} buttons={buttonsIndex} label="Tipos de indexação" />
+        <ButtonsGroup handleClick={handleClick} defaultButton={defaultBtnRendimento} selectedBtn={selectedBtn} buttons={buttonsRendimento} label="Rendimento" />
+        <ButtonsGroup handleClick={handleClick} selectedBtn={selectedBtn} buttons={buttonsIndex} label="Tipos de indexação" />
         <Input handleInputChange={handleInputChange} inputFields={inputFields} />
         <div className="indicadores">
           <p>IPCA (ao ano)</p>
@@ -80,6 +88,8 @@ export const Simulator = () => {
       </div>
       <button type="reset" className="reset-btn">Limpar campos</button>
       <button type="submit" className="submit-btn">Simular</button>
+
+      {/* <Results simulationResult={simulationResult} /> */}
     </form>
   )
 }
