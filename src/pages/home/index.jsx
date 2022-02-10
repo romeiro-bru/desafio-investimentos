5922d88bf6c783faf13a05996a491374a2bee783
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-10 17:52:05 -0300

    Add simulation and filtered simulation, Add coditional render to Results

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,20 @@
 import './style.css';
+import { useState } from 'react';
 import { Simulator } from './simulator';
-import { Results } from '../home/results';
+import { Results } from './results/results';
 
 export function Home() {
+  const [simulations, setSimulations] = useState([])
+  const [filteredSimulation, setFilteredSimulation] = useState([])
+
   return (
     <div>
       <h1>Simulador de Investimentos</h1>
       <div className="wrapper">
-        <Simulator />
-        <Results />
+        <Simulator simulations={simulations} setSimulations={setSimulations} setFilteredSimulation={setFilteredSimulation} />
+        {filteredSimulation.length === 0 ? "" :
+          <Results filteredSimulation={filteredSimulation} />
+        }
       </div>
     </div>
   )
