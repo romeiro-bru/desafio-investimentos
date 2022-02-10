891528c2c1a7b8cc62a67c65f7d66ae0198c8c83
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-10 17:53:17 -0300

    Filter simulation results

--- src/pages/home/results/results.jsx
+++ src/pages/home/results/results.jsx
@@ -1,19 +1,38 @@
-export const Results = ({ simulations, filteredSimulations }) => {
-  console.log("results", simulations)
-  console.log("filtered", filteredSimulations)
+import './style.css';
+
+export const Results = ({ filteredSimulation }) => {
 
   return (
     <section className="results">
       <h2>Resultado da Simulação</h2>
-      <ul>
-        <li>{filteredSimulations[0].valorFinalBruto}</li>
-        <li>{filteredSimulations[0].aliquoaIR}</li>
-        <li>{filteredSimulations[0].valorPagoIR}</li>
+      <ul className="results-list">
+        <li>
+          <p>Valor Final Bruto</p>
+          <p>R$ {filteredSimulation[0].valorFinalBruto}
+          </p>
+        </li>
+        <li>
+          <p>Alíquota do IR</p>
+          <p>{filteredSimulation[0].aliquoaIR === undefined && 0}%</p>
+        </li>
+        <li>
+          <p>Valor Pago em IR</p>
+          <p>R$ {filteredSimulation[0].valorPagoIR}</p>
+        </li>
 
-        <li>{filteredSimulations[0].valorFinalLiquido}</li>
-        <li>{filteredSimulations[0].valorTotalInvestido}</li>
-        <li>{filteredSimulations[0].ganhoLiquido}</li>
+        <li>
+          <p>Valor Final Líquido</p>
+          <p className="green-text">R$ {filteredSimulation[0].valorFinalLiquido}</p>
+        </li>
+        <li>
+          <p>Valor Total Investido</p>
+          <p>R$ {filteredSimulation[0].valorTotalInvestido}</p>
+        </li>
+        <li>
+          <p>Ganho Líquido</p>
+          <p className="green-text">R$ {filteredSimulation[0].ganhoLiquido}</p>
+        </li>
       </ul>
-    </section>
+    </section >
   )
 }
