c258cf62da8e911cf4af4a699f9ef44a0b3e2c88
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-15 14:28:56 -0300

    Add input fields validation

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,12 +1,17 @@
 import './style.css';
 
-export const Input = ({ handleInputChange, inputFields }) => {
+export const Input = ({ handleInputChange, inputFields, isValidInput, inputs }) => {
   return (
     <>
       {inputFields.map((input, i) => (
         <div key={i}>
-          <label htmlFor={input.name}>{input.children}</label>
-          <input onChange={handleInputChange} type="text" name={input.name} />
+          <label htmlFor={input.name}
+            style={{
+              color: isValidInput && input.name !== Object.keys(inputs) ?
+                "" : "red"
+            }}
+          >{input.children}</label>
+          <input onChange={handleInputChange} type="text" pattern="[0-9]*" name={input.name} />
         </div>
       ))}
     </>
