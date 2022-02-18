71945491e3b16fa1a10d9f5c0dc2c27c7840655c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-02-17 21:41:34 -0300

    Remove variable, remove value

--- src/Components/Simulator/Input/Input.jsx
+++ src/Components/Simulator/Input/Input.jsx
@@ -7,7 +7,7 @@ const inputFields = [
   { name: "rentabilidade", children: "Rentabilidade" },
 ]
 
-export const Input = ({ handleInputChange, isValidInput, inputs }) => {
+export const Input = ({ handleInputChange, isValidInput }) => {
 
   return (
     <>
@@ -19,7 +19,6 @@ export const Input = ({ handleInputChange, isValidInput, inputs }) => {
           <input
             type="text"
             name={input.name}
-            value={inputs[input.name] || ""}
             onChange={handleInputChange}
             style={{ borderBottom: isValidInput ? "" : "1px solid red" }} />
           <span className="error-message"
